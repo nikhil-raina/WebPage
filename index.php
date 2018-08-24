50c84a8eea6a91d803150ce38abc7e6727d6ed83
Nikhil Raina <nxr5013@g.rit.edu>
2018-08-23 20:13:18 -0400

    added all the HTML files

--- index.php
+++ index.php
@@ -1,1 +1,6 @@
 <?php include_once("My_Website_HTML/homePage.html") ;  ?>
+<?php include_once("My_Website_HTML/aboutMe.html") ;  ?>
+<?php include_once("My_Website_HTML/academics.html") ;  ?>
+<?php include_once("My_Website_HTML/contactInfo.html") ;  ?>
+<?php include_once("My_Website_HTML/extracurricular.html") ;  ?>
+<?php include_once("My_Website_HTML/projects.html") ;  ?>
